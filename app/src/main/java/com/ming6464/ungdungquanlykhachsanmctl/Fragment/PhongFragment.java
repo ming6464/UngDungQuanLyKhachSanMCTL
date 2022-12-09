e737fe20882b4eff111ea78981d159f931e31b0b
nguyenming <nguyenming.6464@gmail.com>
2022-12-10 01:19:43 +0700

    hoàn thành chức năng

--- app/src/main/java/com/ming6464/ungdungquanlykhachsanmctl/Fragment/PhongFragment.java
+++ app/src/main/java/com/ming6464/ungdungquanlykhachsanmctl/Fragment/PhongFragment.java
@@ -2,43 +2,29 @@ package com.ming6464.ungdungquanlykhachsanmctl.Fragment;
 
 import android.app.AlertDialog;
 import android.app.DatePickerDialog;
-import android.app.Dialog;
-import android.app.TimePickerDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
-import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.widget.LinearLayoutCompat;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Handler;
-import android.util.Log;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.Window;
-import android.view.WindowManager;
-import android.widget.Button;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.DatePicker;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.widget.TimePicker;
-import android.widget.Toast;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.ming6464.ungdungquanlykhachsanmctl.Adapter.RoomsAdapter;
 import com.ming6464.ungdungquanlykhachsanmctl.ChucNangDatPhongActivity;
-import com.ming6464.ungdungquanlykhachsanmctl.CustomToast;
 import com.ming6464.ungdungquanlykhachsanmctl.DTO.Rooms;
 import com.ming6464.ungdungquanlykhachsanmctl.KhachSanDAO;
 import com.ming6464.ungdungquanlykhachsanmctl.KhachSanDB;
@@ -54,13 +40,17 @@ import java.util.List;
 public class PhongFragment extends Fragment implements RoomsAdapter.IClickItemRooms, View.OnClickListener {
     public RecyclerView rcvRooms;
     private RoomsAdapter roomsAdapter;
-    private List<Rooms> mListRooms;
+    private List<Rooms> mListRooms,currentListRooms;
     private SimpleDateFormat sdf,sdf1;
     private Calendar calendar;
     private Date d_checkIn,d_checkOut;
     private ProgressBar pb_load;
+    private ImageView img_currentCategory;
+    private int currentCategory = 2;
+    private boolean checkShowFilterCategory = false;
+    private Animation animation,animationRotate;
     private long time = 0;
-    private TextView tv_checkIn,tv_checkOut;
+    private TextView tv_checkIn,tv_checkOut,tv_singleRoom,tv_doubleRoom,tv_vipRoom;
     public static final String KEY_BUNDLE = "KEY_BUNDLE",KEY_ROOM ="KEY_ROOM",
             KEY_CHECKIN = "KEY_CHECKIN",KEY_CHECKOUT ="KEY_CHECKOUT",
             KEY_STATUS = "KEY_STATUS",KEY_AMOUNT_DATE = "KEY_AMOUNT_DATE";
@@ -85,16 +75,22 @@ public class PhongFragment extends Fragment implements RoomsAdapter.IClickItemRo
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        animationRotate = AnimationUtils.loadAnimation(requireContext(),R.anim.scale_filter_room);
         calendar = Calendar.getInstance();
         d_checkOut = new Date();
         d_checkIn = new Date();
-        sdf = new SimpleDateFormat("dd/MM/yy");
-        sdf1 = new SimpleDateFormat("dd/MM/yy HH");
+        sdf = new SimpleDateFormat("dd/MM/yyyy");
+        sdf1 = new SimpleDateFormat("dd/MM/yyyy HH");
         dao = KhachSanDB.getInstance(getContext()).getDAO();
         tv_checkIn = view.findViewById(R.id.fragPhong_tv_checkIn);
         tv_checkOut = view.findViewById(R.id.fragPhong_tv_checkOut);
         rcvRooms = view.findViewById(R.id.rcv_rooms);
         pb_load= view.findViewById(R.id.fragPhong_pg_load);
+        img_currentCategory = view.findViewById(R.id.fragPhong_img_currentCategory);
+        tv_singleRoom = view.findViewById(R.id.fragPhong_tv_singleRoom);
+        tv_doubleRoom = view.findViewById(R.id.fragPhong_tv_doubleRoom);
+        tv_vipRoom = view.findViewById(R.id.fragPhong_tv_vipRoom);
+        currentListRooms = new ArrayList<>();
         roomsAdapter = new RoomsAdapter(this);
         mListRooms = new ArrayList<>();
         GridLayoutManager gridLayoutManager = new GridLayoutManager(getContext(),3);
@@ -107,6 +103,8 @@ public class PhongFragment extends Fragment implements RoomsAdapter.IClickItemRo
     @Override
     public void onResume() {
         super.onResume();
+        currentCategory = 2;
+        hideFilterCategory();
         try {
             time = sdf.parse(sdf.format(new Date(System.currentTimeMillis()))).getTime();
         } catch (ParseException e) {
@@ -147,18 +145,21 @@ public class PhongFragment extends Fragment implements RoomsAdapter.IClickItemRo
                     .show();
         }
     }
-    private void showLoad(){
+    private void beginLoad(){
         rcvRooms.setVisibility(View.GONE);
         pb_load.setVisibility(View.VISIBLE);
+    }
+    private void endLoad(){
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 rcvRooms.setVisibility(View.VISIBLE);
                 pb_load.setVisibility(View.GONE);
             }
-        },700);
+        },600);
     }
     private void handleFilterRoom(){
+        beginLoad();
         try {
             d_checkIn = sdf1.parse(tv_checkIn.getText().toString() + " 14");
             d_checkOut = sdf1.parse(tv_checkOut.getText().toString() + " 12");
@@ -166,10 +167,22 @@ public class PhongFragment extends Fragment implements RoomsAdapter.IClickItemRo
             e.printStackTrace();
         }
         mListRooms = dao.getListRoomWithTime(d_checkIn,d_checkOut);
-        showLoad();
-        List<String> listCategory = dao.getListNameCategoryWithRoomId(mListRooms);
-        roomsAdapter.setData(mListRooms, listCategory);
+        handleFilterCategory();
     }
+
+    private void handleFilterCategory() {
+        beginLoad();
+        currentListRooms.clear();
+        for(Rooms x : mListRooms){
+            if(x.getCategoryID() == currentCategory)
+                currentListRooms.add(x);
+        }
+        checkShowFilterCategory = false;
+        roomsAdapter.setData(currentListRooms, dao.getListNameCategoryWithRoomId(currentListRooms));
+        updateColorFilterCategory();
+        endLoad();
+    }
+
     private String formatDate(int date){
         if(date < 10)
             return "0" + date;
@@ -179,6 +192,10 @@ public class PhongFragment extends Fragment implements RoomsAdapter.IClickItemRo
     private void handleAction() {
         tv_checkIn.setOnClickListener(this);
         tv_checkOut.setOnClickListener(this);
+        img_currentCategory.setOnClickListener(this);
+        tv_vipRoom.setOnClickListener(this);
+        tv_singleRoom.setOnClickListener(this);
+        tv_doubleRoom.setOnClickListener(this);
     }
 
     @Override
@@ -189,24 +206,92 @@ public class PhongFragment extends Fragment implements RoomsAdapter.IClickItemRo
                 DatePickerDialog datePickerDialog = new DatePickerDialog(requireContext(), new DatePickerDialog.OnDateSetListener() {
                     @Override
                     public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                        tv_checkIn.setText(formatDate(dayOfMonth) + "/" + (month + 1) + "/" + String.valueOf(year).substring(2));
+                        tv_checkIn.setText(formatDate(dayOfMonth) + "/" + (month + 1) + "/" + year);
                     }
                 }, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
                 datePickerDialog.getDatePicker().setMinDate(time);
                 datePickerDialog.show();
                 break;
-            default:
+            case R.id.fragPhong_tv_checkOut:
                 calendar.setTime(d_checkOut);
                 datePickerDialog = new DatePickerDialog(requireContext(), new DatePickerDialog.OnDateSetListener() {
                     @Override
                     public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                        tv_checkOut.setText(formatDate(dayOfMonth) + "/" + (month + 1) + "/" + String.valueOf(year).substring(2));
+                        tv_checkOut.setText(formatDate(dayOfMonth) + "/" + (month + 1) + "/" + year);
                         handleFilterRoom();
                     }
                 },calendar.get(Calendar.YEAR),calendar.get(Calendar.MONTH),calendar.get(Calendar.DAY_OF_MONTH));
                 datePickerDialog.getDatePicker().setMinDate(time);
                 datePickerDialog.show();
                 break;
+            case R.id.fragPhong_img_currentCategory:
+                if(checkShowFilterCategory)
+                    checkShowFilterCategory = false;
+                else
+                    checkShowFilterCategory = true;
+                img_currentCategory.startAnimation(animationRotate);
+                updateShowHideFilterCategory();
+                break;
+            case R.id.fragPhong_tv_singleRoom:
+                if(currentCategory != 1){
+                    currentCategory = 1;
+                    handleFilterCategory();
+                    updateShowHideFilterCategory();
+                }
+                break;
+            case R.id.fragPhong_tv_doubleRoom:
+                if(currentCategory != 2){
+                    currentCategory = 2;
+                    handleFilterCategory();
+                    updateShowHideFilterCategory();
+                }
+                break;
+            default:
+                if(currentCategory != 3){
+                    currentCategory = 3;
+                    handleFilterCategory();
+                    updateShowHideFilterCategory();
+                }
+
         }
+    }
+
+    private void updateColorFilterCategory() {
+        switch (currentCategory){
+            case 1:
+                img_currentCategory.setImageResource(R.drawable.single_user_30);
+                tv_singleRoom.setBackgroundResource(R.color.hoadon_chuathanhtoan);
+                tv_doubleRoom.setBackgroundResource(R.color.hoadon_thanhtoan);
+                tv_vipRoom.setBackgroundResource(R.color.hoadon_thanhtoan);
+                break;
+            case 2:
+                img_currentCategory.setImageResource(R.drawable.double_user_30);
+                tv_singleRoom.setBackgroundResource(R.color.hoadon_thanhtoan);
+                tv_doubleRoom.setBackgroundResource(R.color.hoadon_chuathanhtoan);
+                tv_vipRoom.setBackgroundResource(R.color.hoadon_thanhtoan);
+                break;
+            default:
+                img_currentCategory.setImageResource(R.drawable.vip_29);
+                tv_singleRoom.setBackgroundResource(R.color.hoadon_thanhtoan);
+                tv_doubleRoom.setBackgroundResource(R.color.hoadon_thanhtoan);
+                tv_vipRoom.setBackgroundResource(R.color.hoadon_chuathanhtoan);
+        }
+    }
+
+    private void updateShowHideFilterCategory(){
+        if(checkShowFilterCategory)
+            animation = AnimationUtils.loadAnimation(requireContext(),R.anim.show_category);
+        else
+            animation = AnimationUtils.loadAnimation(requireContext(),R.anim.hide_category);
+        tv_singleRoom.startAnimation(animation);
+        tv_doubleRoom.startAnimation(animation);
+        tv_vipRoom.startAnimation(animation);
+    }
+
+    private void hideFilterCategory(){
+        animation = AnimationUtils.loadAnimation(requireContext(),R.anim.hide_1_category);
+        tv_singleRoom.startAnimation(animation);
+        tv_doubleRoom.startAnimation(animation);
+        tv_vipRoom.startAnimation(animation);
     }
 }
